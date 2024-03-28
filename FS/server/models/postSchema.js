0433cb6eaf3189a19a84e2fa6eb539449f4131c6
Ravij-p <ravijbpatel9124@gmail.com>
2024-03-28 17:57:20 +0530

    Talkies the social media App

---
+++ FS/server/models/postSchema.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const postSchema = new mongoose.Schema({
+  caption: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "USER",
+    required: true,
+  },
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "USER",
+    },
+  ],
+  comment: [
+    {
+      commentUser: {
+        type: String,
+      },
+      msg: {
+        type: String,
+      },
+    },
+  ],
+  save: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "USER",
+    },
+  ],
+});
+const Post = new mongoose.model("POST", postSchema);
+module.exports = Post;
